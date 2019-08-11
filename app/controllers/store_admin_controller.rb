6be19279f9f4c1fe95c3f621feac668ae8c56fb7
Ryan Pethel <arpethel@gmail.com>
2019-08-11 19:17:54 +0000

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,1 +1,14 @@
-<h1>Hello World!</h1>
+class StoreAdminController < ApplicationController
+  layout 'admin'
+
+  def home
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def invoice
+    render :layout => false
+  end
+end
